430dc05f7e7c7d2ee3b3483d4bf0342e8608a311
pwh0607 <120497748+pwh0607@users.noreply.github.com>
2024-08-21 17:53:46 +0900

    컨트롤러 추가 커밋(1)
    Login 컨트롤러 추가

---
+++ RogueRunnerServer/RogueRunnerServer/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace RogueRunnerServer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController
+    {
+    }
+}
+public class LoginRequest
+{
+    public string Id;
+    public string Password;
+}
