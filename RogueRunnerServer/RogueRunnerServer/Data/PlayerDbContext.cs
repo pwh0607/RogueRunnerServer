ad11a9a74485988f7e00cb9398e40961ac47a841
pwh0607 <120497748+pwh0607@users.noreply.github.com>
2024-08-27 00:05:42 +0900

    DB 참조 추가
    사용자 정보, 게임 내 캐릭터 정보 매핑용 Entity 추가

---
+++ RogueRunnerServer/RogueRunnerServer/Data/PlayerDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RogueRunnerServer.Controllers;
+using RogueRunnerServer.Model;
+
+namespace RogueRunnerServer.Data
+{
+    public class PlayerDbContext : DbContext
+    {
+        public DbSet<PlayerData> PlayerDatas { get; set; }
+
+        public PlayerDbContext(DbContextOptions<PlayerDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PlayerData>().ToTable("playerdata");
+            modelBuilder.Entity<PlayerData>().HasKey(u => u.P_Id);
+            modelBuilder.Entity<PlayerData>().Property(u => u.Stage).IsRequired();
+            modelBuilder.Entity<PlayerData>().Property(u => u.SceneName).IsRequired();
+            modelBuilder.Entity<PlayerData>().Property(u => u.PlayerCharacter).IsRequired();
+            modelBuilder.Entity<PlayerData>().Property(u => u.HP).IsRequired();
+            modelBuilder.Entity<PlayerData>().Property(u => u.Score).IsRequired();
+            modelBuilder.Entity<PlayerData>().Property(u => u.Speed).IsRequired();
+            modelBuilder.Entity<PlayerData>().Property(u => u.Skills).IsRequired();
+        }
+    }
+}
---
+++ RogueRunnerServer/RogueRunnerServer/Data/UserDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RogueRunnerServer.Controllers;
+using RogueRunnerServer.Model;
+
+namespace RogueRunnerServer.Data
+{
+    //DB의 테이블과 클라이언트로 부터 받아온 값을 정리.
+    public class UserDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public UserDbContext(DbContextOptions<UserDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>().ToTable("userdata");
+            modelBuilder.Entity<User>().HasKey(u => u.P_Id);
+            modelBuilder.Entity<User>().Property(u => u.Id).IsRequired();
+            modelBuilder.Entity<User>().Property(u => u.PasswordHash).IsRequired();
+            modelBuilder.Entity<User>().Property(u => u.Nickname).IsRequired();
+        }
+    }
+}
