27d8fdf06c0436e29f6e8ce9bcedb7a258f169e9
pwh0607 <120497748+pwh0607@users.noreply.github.com>
2024-08-27 00:06:02 +0900

    모델 추가 커밋
    게임 내 캐릭터 Stat에 대한 모델 추가

--- RogueRunnerServer/RogueRunnerServer/Model/PlayerData.cs
+++ RogueRunnerServer/RogueRunnerServer/Model/PlayerData.cs
@@ -1,17 +1,18 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System.ComponentModel.DataAnnotations.Schema;
+using Newtonsoft.Json;
 
 namespace RogueRunnerServer.Model
 {
     public class PlayerData
     {
-        public int P_Id { get; set; }
+        public string P_Id { get; set; }
         public int Stage { get; set; }                      //Stage (오름차)
         public string SceneName { get; set; }               //실행할 씬 이름
         public string PlayerCharacter { get; set; }         //사용할 캐릭터 명
         public int HP { get; set; }
         public float Score { get; set; }
         public float Speed { get; set; }
-        public Dictionary<string, int> Skills { get; set; }
+        public string Skills { get; set; }
     }
 }
