860db6ce30970765237f2ca0510f6fd661356c27
pwh0607 <120497748+pwh0607@users.noreply.github.com>
2024-09-19 21:12:34 +0900

    CS 수정 커밋
    model anotation 추가

--- RogueRunnerServer/RogueRunnerServer/Model/PlayerData.cs
+++ RogueRunnerServer/RogueRunnerServer/Model/PlayerData.cs
@@ -6,13 +6,28 @@ namespace RogueRunnerServer.Model
 {
     public class PlayerData
     {
+        [Column("P_id")]
         public string P_Id { get; set; }
-        public int Stage { get; set; }                      
-        public string SceneName { get; set; }               
-        public string PlayerCharacter { get; set; }         
+
+        [Column("Stage")]
+        public int Stage { get; set; }
+
+        [Column("SceneName")]
+        public string SceneName { get; set; }
+
+        [Column("PlayerCharacter")]
+        public string PlayerCharacter { get; set; }
+
+        [Column("HP")]
         public int HP { get; set; }
+
+        [Column("Score")]
         public float Score { get; set; }
+
+        [Column("Speed")]
         public float Speed { get; set; }
+
+        [Column("Skills")]
         public string Skills { get; set; }
     }
 }
--- RogueRunnerServer/RogueRunnerServer/Model/ScoreRank.cs
+++ RogueRunnerServer/RogueRunnerServer/Model/ScoreRank.cs
@@ -1,10 +1,18 @@
-﻿namespace RogueRunnerServer.Model
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RogueRunnerServer.Model
 {
     public class ScoreRank
     {
         //플레이어가 게임 종료 되었을 때, 랭킹 갱신용.
+
+        [Column("p_id")]
         public string P_Id { get; set; }
+
+        [Column("nickname")]
         public string Nickname { get; set; }
+
+        [Column("score")]
         public float Score { get; set; }
     }
 }
