9fe247f47e3bad033b65b134316677e16cc25ef3
pwh0607 <120497748+pwh0607@users.noreply.github.com>
2024-10-16 23:58:25 +0900

    Controller 수정 커밋(1)
    p_id 생성 로직 수정 및 주석 제거

--- RogueRunnerServer/RogueRunnerServer/Controllers/RegisterController.cs
+++ RogueRunnerServer/RogueRunnerServer/Controllers/RegisterController.cs
@@ -7,7 +7,9 @@ using System.Reflection;
 using Microsoft.EntityFrameworkCore;
 using RogueRunnerServer.Model;
 using RogueRunnerServer.Data;
+using RogueRunnerServer.Service;
 using System.Data;
+using System.Reflection.Metadata.Ecma335;
 
 namespace RogueRunnerServer.Controllers
 {
@@ -27,39 +29,37 @@ namespace RogueRunnerServer.Controllers
         public async Task<IActionResult> RegisterUser([FromBody] RegisterRequest request)
         {
             Console.WriteLine("POST : Register Post Request...");
-            // 기본적인 데이터 유효성 검사
+
             if (string.IsNullOrEmpty(request.Id) || string.IsNullOrEmpty(request.Nickname) || string.IsNullOrEmpty(request.Password)){
                 return BadRequest("모든 데이터를 넣어주세요...");
             }
 
             Console.WriteLine($"요청 정보 - UserID : {request.Id}, Password : {request.Password}, Nickname : {request.Nickname},");
 
-            // 사용자 ID 중복 검사
             if (await IsUserIdDuplicated(request.Id)){
                 return Conflict(new { message = "이미 사용 중인 ID입니다." });
             }
-            // 닉네임 중복 검사
+
             if (await IsNicknameDuplicated(request.Nickname)){
                 return Conflict(new { message = "이미 사용 중인 NickName입니다." });
             }
-
-            string p_id = System.Guid.NewGuid().ToString();             // 고유 식별번호 생성
-            var passwordHasher = new PasswordHasher<User>();            //비밀번호 해싱
+            string curYear = DateTime.Today.Year.ToString();
+            int cnt = _context.Users.Count(u => u.P_Id.StartsWith(curYear));
+            string p_id = PidService.MakePid(cnt);
+            var passwordHasher = new PasswordHasher<User>();
             var hashedPassword = passwordHasher.HashPassword(null, request.Password);
 
-            var newUser = new User(p_id, request.Id, hashedPassword, request.Nickname); // 사용자 데이터 모델 생성
+            var newUser = new User(p_id, request.Id, hashedPassword, request.Nickname); 
 
             _context.Users.Add(newUser);
             await _context.SaveChangesAsync();
             return Ok(new { message = "회원 가입 성공.", user = newUser });
         }
 
-        // 사용자 ID 중복 검사 메서드
         private async Task<bool> IsUserIdDuplicated(string userId){
             return await _context.Users.AnyAsync(u => u.Id == userId);
         }
 
-        // 닉네임 중복 검사 메서드
         private async Task<bool> IsNicknameDuplicated(string nickname){
             return await _context.Users.AnyAsync(u => u.Nickname == nickname);
         }
---
+++ RogueRunnerServer/RogueRunnerServer/Service/PidService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Specialized;
+
+namespace RogueRunnerServer.Service
+{
+    public class PidService
+    {
+        public static string MakePid(int cnt)
+        {
+            DateTime today = DateTime.Today;
+            string year = (today.Year % 100).ToString();
+            string userNum = (cnt+1).ToString();
+            string pidTail = userNum.PadLeft(10 - userNum.Length + 1, '0');
+            string newPid = year + "-" + pidTail;
+
+            return newPid;
+        }
+    }
+}
